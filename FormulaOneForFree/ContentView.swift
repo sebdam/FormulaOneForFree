c0a9c4746c033fd14ca319f1f63b70872b085fe4
SebDam <sebastien@damiens-cerf.com>
2024-10-19 10:30:43 +0200

    Initial Commit

---
+++ FormulaOneForFree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormulaOneForFree
+//
+//  Created by Sébastien Damiens-Cerf on 19/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormulaOneForFree/FormulaOneForFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneForFreeApp.swift
+//  FormulaOneForFree
+//
+//  Created by Sébastien Damiens-Cerf on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneForFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
